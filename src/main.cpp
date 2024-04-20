182dc89e3c4f470ce1f87cf2acf9f1fb9b31d6c4
JoelSaleem <40210575+JoelSaleem@users.noreply.github.com>
2024-04-20 11:15:23 +0100

    :sparkles: boiler plate for get /

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,35 @@
 #include <iostream>
+#include <cpprest/http_listener.h>
+#include <cpprest/json.h>
+
+void handle_get(web::http::http_request request)
+{
+    ucout << "Received a GET request\n";
+    web::json::value response_data;
+    response_data[U("message")] = web::json::value::string(U("Hello, World!"));
+
+    request.reply(web::http::status_codes::OK, response_data);
+}
 
 int main()
 {
-    std::cout << "hello world" << std::endl;
+    web::uri_builder uri(U("http://localhost:8080"));
+    std::string addr = uri.to_uri().to_string();
+
+    web::http::experimental::listener::http_listener listener(addr);
+    listener.support(web::http::methods::GET, handle_get);
+
+    try {
+        listener
+            .open()
+            .then([&listener]() { ucout << "Starting to listen at: " << listener.uri().to_string() << std::endl; })
+            .wait();
+
+        std::string line;
+        std::getline(std::cin, line);
+    } catch (const std::exception &e) {
+        std::cerr << "An error occurred: " << e.what() << std::endl;
+    }
+
     return 0;
 }
