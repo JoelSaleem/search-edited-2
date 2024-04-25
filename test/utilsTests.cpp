11136883e99ece71d19b783896965c54eafd2495
JoelSaleem <40210575+JoelSaleem@users.noreply.github.com>
2024-04-25 16:04:35 +0200

    :white_check_mark: more tests for utils::getPath()

--- test/utilsTests.cpp
+++ test/utilsTests.cpp
@@ -1,8 +1,32 @@
 #include <gtest/gtest.h>
 #include "utils.h"
 
-TEST(UtilsTests, HandlesZeroInput)
+TEST(UtilsTests, ValidBasicPath)
 {
     std::string path = *utils::getPath("/abc");
     EXPECT_EQ(path, "/abc");
+}
+
+TEST(UtilsTests, ZeroLenPath)
+{
+
+    EXPECT_THROW(utils::getPath(""), std::runtime_error);
+}
+
+TEST(UtilsTests, DoesNotStartWithSlash)
+{
+
+    EXPECT_THROW(utils::getPath("abc"), std::runtime_error);
+}
+
+TEST(UtilsTests, NestedPath)
+{
+    std::string path = *utils::getPath("/abc/myResource");
+    EXPECT_EQ(path, "/abc/myResource");
+}
+
+TEST(UtilsTests, NestedPathWithQueryParams)
+{
+    std::string path = *utils::getPath("/abc/myResource?key=val&key2=val2");
+    EXPECT_EQ(path, "/abc/myResource");
 }
